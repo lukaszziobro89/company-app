11c097b8ba3204c33d1397ab14b1cd5c379a96c7
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2018-12-29 12:11:56 +0100

    Equals and hascode methods tests

--- src/test/java/com/mycompany/app/Employee_Test.java
+++ src/test/java/com/mycompany/app/Employee_Test.java
@@ -4,14 +4,14 @@ import com.mycompany.entity.DepartmentType;
 import com.mycompany.entity.Employee;
 import com.mycompany.exceptions.AgeException;
 import com.mycompany.exceptions.ChangeSalaryException;
-import com.mycompany.exceptions.SalaryException;
+import com.mycompany.exceptions.NegativeSalaryException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class Employee_Test {
 
@@ -41,6 +41,62 @@ public class Employee_Test {
     }
 
     @Test
+    public void TEST_toString(){
+        Employee employee = new Employee(
+                "Name",
+                "Surname",
+                33,
+                5000,
+                "mail@mail.com",
+                DepartmentType.Marketing,
+                Arrays.asList("English", "Spanish")
+        );
+        String expected = "Employee{id=0, name='Name', surname='Surname', age=33, salary=5000.0, email='mail@mail.com', department=Marketing, languages=[English, Spanish]}";
+        String actual = employee.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TEST_hashCode(){
+        Employee employee = new Employee(
+                "Name",
+                "Surname",
+                33,
+                5000,
+                "mail@mail.com",
+                DepartmentType.Marketing,
+                Arrays.asList("English", "Spanish")
+        );
+
+        Employee employee1 = new Employee(
+                "Name",
+                "Surname",
+                33,
+                5000,
+                "mail@mail.com",
+                DepartmentType.Marketing,
+                Arrays.asList("English", "Spanish")
+        );
+
+        Employee employee2 = new Employee(
+                "Name2",
+                "Surname2",
+                34,
+                6000,
+                "mail2@mail.com",
+                DepartmentType.Marketing,
+                Collections.singletonList("English")
+        );
+
+        assertEquals(employee, employee1);
+        assertNotEquals(employee, employee2);
+
+        assertEquals(employee.hashCode(), employee1.hashCode());
+        assertNotEquals(employee.hashCode(), employee2.hashCode());
+    }
+
+    @Test
     public void TEST_throwEmployeeAgeException(){
         expectedException.expect(AgeException.class);
         expectedException.expectMessage("Age cannot be negative or zero.");
@@ -56,7 +112,7 @@ public class Employee_Test {
 
     @Test
     public void TEST_throwEmployeeSalaryException(){
-        expectedException.expect(SalaryException.class);
+        expectedException.expect(NegativeSalaryException.class);
         expectedException.expectMessage("Salary cannot be negative.");
         Employee employee = new Employee(
                 "TestName",
