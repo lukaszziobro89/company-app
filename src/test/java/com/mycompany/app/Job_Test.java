6510b6dd284d420aa4654569ab1ee1945bb8c7a5
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2018-12-30 16:24:34 +0100

    Job constructor test

--- src/test/java/com/mycompany/app/Job_Test.java
+++ src/test/java/com/mycompany/app/Job_Test.java
@@ -1,4 +1,26 @@
 package com.mycompany.app;
 
+import com.mycompany.entity.DepartmentType;
+import com.mycompany.entity.Job;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
 public class Job_Test {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void TEST_createJobConstructor(){
+        Job job = new Job(10, "Java Developer", 5000, 20000, DepartmentType.IT);
+        Job job1 = new Job(20, "Java Developer", 5000, 20000, DepartmentType.IT);
+        Job job2 = new Job(30, "Java Developer", 6000, 20000, DepartmentType.IT);
+        assertEquals(10, job.getJobId());
+        assertEquals(20, job1.getJobId());
+        assertEquals(30, job2.getJobId());
+    }
+
+
 }
