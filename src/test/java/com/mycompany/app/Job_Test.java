54057e8437bf5dd02cc529867e39f764575c788a
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2018-12-30 16:36:59 +0100

    Equals method test

--- src/test/java/com/mycompany/app/Job_Test.java
+++ src/test/java/com/mycompany/app/Job_Test.java
@@ -2,11 +2,13 @@ package com.mycompany.app;
 
 import com.mycompany.entity.DepartmentType;
 import com.mycompany.entity.Job;
+import junit.framework.TestCase;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;
 
 public class Job_Test {
     @Rule
@@ -20,6 +22,15 @@ public class Job_Test {
         assertEquals(10, job.getJobId());
         assertEquals(20, job1.getJobId());
         assertEquals(30, job2.getJobId());
+    }
+
+    @Test
+    public void TEST_equalsMethod(){
+        Job job = new Job(10, "Java Developer", 5000, 20000, DepartmentType.IT);
+        Job job1 = new Job(20, "Java Developer", 5000, 20000, DepartmentType.IT);
+        Job job2 = new Job(30, "Java Developer", 6000, 20000, DepartmentType.IT);
+        assertEquals(job, job1);
+        assertNotEquals(job1, job2);
     }
 
 
