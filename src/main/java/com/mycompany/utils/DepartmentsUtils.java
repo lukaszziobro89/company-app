ecb2aee597c20d68a81b3f3bd0b6b76b08e04795
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-04 15:55:22 +0100

    countAverageSalaryForDepartment

--- src/main/java/com/mycompany/utils/DepartmentsUtils.java
+++ src/main/java/com/mycompany/utils/DepartmentsUtils.java
@@ -1,4 +1,14 @@
 package com.mycompany.utils;
 
+import com.mycompany.entity.Department;
+import com.mycompany.entity.Employee;
+
 public class DepartmentsUtils {
+
+    public double countAverageSalaryForDepartment(Department department){
+        return department.getEmployeeList().stream()
+                .mapToDouble(Employee::getSalary)
+                .average()
+                .orElse(Double.NaN);
+    }
 }
