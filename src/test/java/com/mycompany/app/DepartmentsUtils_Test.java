ab8504106301e014128fb78e0d24c8a4d005c120
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-04 15:56:07 +0100

    countAverageSalaryForDepartment test

--- src/test/java/com/mycompany/app/DepartmentsUtils_Test.java
+++ src/test/java/com/mycompany/app/DepartmentsUtils_Test.java
@@ -1,4 +1,47 @@
 package com.mycompany.app;
 
+import com.mycompany.entity.Department;
+import com.mycompany.entity.DepartmentType;
+import com.mycompany.entity.Employee;
+import com.mycompany.utils.DepartmentsUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
 public class DepartmentsUtils_Test {
+
+    private DepartmentsUtils departmentsUtils = new DepartmentsUtils();
+
+    @Test
+    public void TEST_countAverageSalaryForDepartment(){
+        Department FinanceDepartment = new Department(DepartmentType.Finance);
+        Employee employee = new Employee(
+                "FirstName",
+                "LastName",
+                33,
+                3000,
+                "mail@mail.com",
+                DepartmentType.Executive,
+                Collections.singletonList("English"));
+
+        Employee employee1 = new Employee(
+                "FirstName1",
+                "LastName1",
+                32,
+                6000,
+                "mail1@mail.com",
+                DepartmentType.IT_Helpdesk,
+                Arrays.asList("English", "Spanish"));
+
+        FinanceDepartment.addEmployeeToEmployeesList(employee);
+        FinanceDepartment.addEmployeeToEmployeesList(employee1);
+
+        double assertValue = 4500;
+        double averageFinanceSalary = departmentsUtils.countAverageSalaryForDepartment(FinanceDepartment);
+
+        assertEquals(assertValue, averageFinanceSalary,0);
+    }
 }
