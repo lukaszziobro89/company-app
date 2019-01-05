0b9922eeb74f44adc2ede018a4226d9101a5cf25
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-05 12:52:16 +0100

    EmployeesUtils test - before class method

---
+++ src/test/java/com/mycompany/app/EmployeesUtils_Test.java
@@ -0,0 +1,38 @@
+package com.mycompany.app;
+
+import com.mycompany.entity.Department;
+import com.mycompany.entity.DepartmentType;
+import com.mycompany.entity.Employee;
+import com.mycompany.utils.DepartmentsUtils;
+import org.junit.BeforeClass;
+
+import java.util.Collections;
+
+public class EmployeesUtils_Test {
+    private DepartmentsUtils departmentsUtils = new DepartmentsUtils();
+    private static Department MarketingDepartment = new Department(DepartmentType.Marketing);
+
+    @BeforeClass
+    public static void createDepartment(){
+        Department marketingDepartment = new Department(DepartmentType.Finance);
+        Employee employee = new Employee("FirstName", "LastName", 33, 3000,
+                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+        Employee employee1 = new Employee("FirstName1", "LastName1", 34, 4000,
+                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+        Employee employee2 = new Employee("FirstName2", "LastName2", 35, 5000,
+                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+        Employee employee3 = new Employee("FirstName3", "LastName3", 36, 6000,
+                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+        Employee employee4 = new Employee("FirstName4", "LastName4", 37, 7000,
+                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+        Employee employee5 = new Employee("FirstName5", "LastName5", 38, 8000,
+                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+        marketingDepartment.addEmployeeToEmployeesList(employee);
+        marketingDepartment.addEmployeeToEmployeesList(employee1);
+        marketingDepartment.addEmployeeToEmployeesList(employee2);
+        marketingDepartment.addEmployeeToEmployeesList(employee3);
+        marketingDepartment.addEmployeeToEmployeesList(employee4);
+        marketingDepartment.addEmployeeToEmployeesList(employee5);
+        MarketingDepartment = marketingDepartment;
+    }
+}
