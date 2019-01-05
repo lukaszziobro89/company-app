5e6daa74e00210e75adedf98949f56b151bb9910
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-05 13:11:59 +0100

    getAllLanguagesEmployeesSpeakFromDepartment

--- src/main/java/com/mycompany/utils/EmployeesUtils.java
+++ src/main/java/com/mycompany/utils/EmployeesUtils.java
@@ -1,4 +1,18 @@
 package com.mycompany.utils;
 
+import com.mycompany.entity.Department;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class EmployeesUtils {
+
+    public List<String> getAllLanguagesEmployeesSpeakFromDepartment(Department department){
+        return department.getEmployeeList().stream()
+                .map(employee -> employee.getLanguages().stream())
+                    .flatMap(Stream::sorted)
+                        .distinct()
+                            .collect(Collectors.toList());
+    }
 }
