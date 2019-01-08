6ff4e70ade92696311efc080db9c3af0db9eea9e
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-08 20:37:17 +0100

    refactoring after adding Job attribute

--- src/test/java/com/mycompany/app/DepartmentsUtils_Test.java
+++ src/test/java/com/mycompany/app/DepartmentsUtils_Test.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 import com.mycompany.entity.Department;
 import com.mycompany.entity.DepartmentType;
 import com.mycompany.entity.Employee;
+import com.mycompany.entity.Job;
 import com.mycompany.utils.DepartmentsUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,22 +15,23 @@ public class DepartmentsUtils_Test {
 
     private DepartmentsUtils departmentsUtils = new DepartmentsUtils();
     private static Department FinanceDepartment = new Department(DepartmentType.Finance);
+    private static Job executiveManager = new Job(40,"Executive Manager", 18000, 20000,DepartmentType.Executive);
 
     @BeforeClass
     public static void createDepartment(){
         Department FinanceDepartement = new Department(DepartmentType.Finance);
         Employee employee = new Employee("FirstName", "LastName", 33, 3000,
-                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+                "mail@mail.com", DepartmentType.Executive, executiveManager, Collections.singletonList("English"));
         Employee employee1 = new Employee("FirstName1", "LastName1", 34, 4000,
-                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+                "mail@mail.com", DepartmentType.Executive, executiveManager, Collections.singletonList("English"));
         Employee employee2 = new Employee("FirstName2", "LastName2", 35, 5000,
-                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+                "mail@mail.com", DepartmentType.Executive, executiveManager, Collections.singletonList("English"));
         Employee employee3 = new Employee("FirstName3", "LastName3", 36, 6000,
-                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+                "mail@mail.com", DepartmentType.Executive, executiveManager, Collections.singletonList("English"));
         Employee employee4 = new Employee("FirstName4", "LastName4", 37, 7000,
-                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+                "mail@mail.com", DepartmentType.Executive, executiveManager, Collections.singletonList("English"));
         Employee employee5 = new Employee("FirstName5", "LastName5", 38, 8000,
-                "mail@mail.com", DepartmentType.Executive, Collections.singletonList("English"));
+                "mail@mail.com", DepartmentType.Executive, executiveManager, Collections.singletonList("English"));
         FinanceDepartement.addEmployeeToEmployeesList(employee);
         FinanceDepartement.addEmployeeToEmployeesList(employee1);
         FinanceDepartement.addEmployeeToEmployeesList(employee2);
