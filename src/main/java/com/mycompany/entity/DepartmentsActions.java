6fdc556cf2393ab86b16777ace6d244c5e6c90f6
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-09 18:37:09 +0100

    create departments actions interface

--- src/main/java/com/mycompany/entity/DepartmentsActions.java
+++ src/main/java/com/mycompany/entity/DepartmentsActions.java
@@ -1,4 +1,9 @@
 package com.mycompany.entity;
 
+import java.util.List;
+
 public interface DepartmentsActions {
+    void addJobToJobList(Job job);
+    void addEmployeeToEmployeesList(Employee employee);
+    void addEmployeeToEmployeesList(List<Employee> employees);
 }
