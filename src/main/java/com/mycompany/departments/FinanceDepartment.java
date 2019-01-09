669f9353bc9029d30e8f8a7df399f79407393712
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-09 18:33:22 +0100

    Add Finance department class

--- src/main/java/com/mycompany/departments/FinanceDepartment.java
+++ src/main/java/com/mycompany/departments/FinanceDepartment.java
@@ -1,4 +1,18 @@
 package com.mycompany.departments;
 
-public class FinanceDepartment {
+import com.mycompany.entity.Department;
+import com.mycompany.entity.DepartmentType;
+import com.mycompany.entity.Employee;
+import com.mycompany.entity.Job;
+
+import java.util.List;
+
+public class FinanceDepartment extends Department {
+    public FinanceDepartment(DepartmentType departmentName, List<Job> jobList, List<Employee> employeesList) {
+        super(departmentName, jobList, employeesList);
+    }
+
+    public FinanceDepartment(DepartmentType departmentName) {
+        super(departmentName);
+    }
 }
