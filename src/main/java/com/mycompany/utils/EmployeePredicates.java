2f0b3a298beb5bbe5fc2691cc09eb9e4c5e1903e
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2019-01-16 20:01:49 +0100

    introduce Predicate interface

--- src/main/java/com/mycompany/utils/EmployeePredicates.java
+++ src/main/java/com/mycompany/utils/EmployeePredicates.java
@@ -1,4 +1,18 @@
 package com.mycompany.utils;
 
+import com.mycompany.entity.Employee;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class EmployeePredicates {
+
+    public static List<Employee> filterEmployees(List<Employee> employeeList, Predicate<Employee> predicate){
+        return employeeList.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+
 }
